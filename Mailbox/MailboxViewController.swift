03b3e776854c076b84ce89f208381c0766de866f
Frank <frank@lyft.com>
2016-02-20 04:40:30 -0800

    showing schedule & list views on pan

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -21,6 +21,8 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var laterImageView: UIImageView!
     @IBOutlet weak var deleteImageView: UIImageView!
     @IBOutlet weak var listImageView: UIImageView!
+    @IBOutlet weak var scheduleView: UIView!
+    @IBOutlet weak var listView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +31,8 @@ class MailboxViewController: UIViewController {
         feedScrollView.contentSize = CGSize(width: 320, height: 1432)
         feedScrollView.frame.size = CGSize(width: 320, height: 568)
         
-        
+        self.scheduleView.alpha = 0
+        self.listView.alpha = 0
         
     }
 
@@ -105,9 +108,62 @@ class MailboxViewController: UIViewController {
 
             let velocity = sender.velocityInView(view)
             
-            if (velocity.x > 0 && translation.x < -200) || (velocity.x > 0 && translation.x > 200) {
-                UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: { () -> Void in
-                    self.singleMessageView.center = CGPoint(x: self.messageOriginalCenter.x + translation.x, y: self.messageOriginalCenter.y)
+            if translation.x < -200 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.backgroundMessageView.backgroundColor = UIColor.init(red: 216/255.0, green: 165/255.0, blue: 117/255.0, alpha: 1.0)
+                    self.laterImageView.alpha = 0
+                    self.listImageView.alpha = 0
+                    self.archiveImageView.alpha = 0
+                    self.deleteImageView.alpha = 0
+                    
+                    self.singleMessageView.center = CGPoint(x: self.messageOriginalCenter.x - 320, y: self.messageOriginalCenter.y)
+                    
+                    self.listView.alpha = 1
+                    
+                    }, completion: { (Bool) -> Void in
+                })
+                
+            } else if translation.x > 200 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    self.backgroundMessageView.backgroundColor = UIColor.init(red: 235/255.0, green: 84/255.0, blue: 51/255.0, alpha: 1.0)
+                    self.laterImageView.alpha = 0
+                    self.listImageView.alpha = 0
+                    self.archiveImageView.alpha = 0
+                    self.deleteImageView.alpha = 0
+                    
+                    self.singleMessageView.center = CGPoint(x: self.messageOriginalCenter.x + 320, y: self.messageOriginalCenter.y)
+                    
+                    }, completion: { (Bool) -> Void in
+                })
+                
+            } else if translation.x < -60 && translation.x > -200 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    
+                    self.backgroundMessageView.backgroundColor = UIColor.init(red: 250/255.0, green: 211/255.0, blue: 51/255.0, alpha: 1.0)
+                    
+                    self.laterImageView.alpha = 0
+                    self.listImageView.alpha = 0
+                    self.archiveImageView.alpha = 0
+                    self.deleteImageView.alpha = 0
+                    
+                    self.singleMessageView.center = CGPoint(x: self.messageOriginalCenter.x - 320, y: self.messageOriginalCenter.y)
+                    
+                    self.scheduleView.alpha = 1
+                    }, completion: { (Bool) -> Void in
+                })
+                
+            } else if translation.x > 60 && translation.x < 200 {
+                UIView.animateWithDuration(0.4, animations: { () -> Void in
+                    
+                    self.backgroundMessageView.backgroundColor = UIColor.init(red: 112/255.0, green: 217/255.0, blue: 98/255.0, alpha: 1.0)
+                    
+                    self.laterImageView.alpha = 0
+                    self.listImageView.alpha = 0
+                    self.archiveImageView.alpha = 0
+                    self.deleteImageView.alpha = 0
+                    
+                    self.singleMessageView.center = CGPoint(x: self.messageOriginalCenter.x + 320, y: self.messageOriginalCenter.y)
+
                     }, completion: { (Bool) -> Void in
                 })
                 
@@ -129,6 +185,16 @@ class MailboxViewController: UIViewController {
         }
     }
 
+    @IBAction func didPressSchedule(sender: AnyObject) {
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            self.scheduleView.alpha = 0
+        })
+    }
+    @IBAction func didPressList(sender: AnyObject) {
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            self.listView.alpha = 0
+        })
+    }
     /*
     // MARK: - Navigation
 
